91080c0ff19131afe497cd32543d1e5bc86753df
Bulat Sharifullin <good1hare@gmail.com>
2018-02-14 13:05:46 +0300

    login

---
+++ app/src/main/java/com/example/bulat/marktask/activities/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.bulat.marktask.activities.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.bulat.marktask.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+}
--- app/src/main/java/com/example/bulat/marktask/activities/main/MainActivity.java
+++ app/src/main/java/com/example/bulat/marktask/activities/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.bulat.marktask;
+package com.example.bulat.marktask.activities.main;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.example.bulat.marktask.R;
 
 public class MainActivity extends AppCompatActivity
     implements NavigationView.OnNavigationItemSelectedListener {
