0bd652dcbbe965b667781c9a8bc2d04c14ff0914
Bulat Sharifullin <good1hare@gmail.com>
2018-02-15 11:07:00 +0300

    sign in button

--- app/src/main/java/com/example/bulat/marktask/activities/login/LoginActivity.java
+++ app/src/main/java/com/example/bulat/marktask/activities/login/LoginActivity.java
@@ -2,9 +2,17 @@ package com.example.bulat.marktask.activities.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import butterknife.BindView;
 import com.example.bulat.marktask.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+  @BindView(R.id.hello_text)
+  TextView tvHelloText;
+  @BindView(R.id.sign_in_button)
+  View signInButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
